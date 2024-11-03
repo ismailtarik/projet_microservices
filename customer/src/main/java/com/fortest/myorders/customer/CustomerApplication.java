51733074dbf8b196a03a4db5dd74553fb719f7bd
ismailtarik <tarikismail600@gmail.com>
2024-11-03 18:33:16 +0100

    C18

--- customer/src/main/java/com/fortest/myorders/customer/CustomerApplication.java
+++ customer/src/main/java/com/fortest/myorders/customer/CustomerApplication.java
@@ -1,7 +1,9 @@
 package com.fortest.myorders.customer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CustomerApplication {
